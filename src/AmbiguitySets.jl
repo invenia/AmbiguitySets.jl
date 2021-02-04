fde1cce9884fd9fd7cab7a440a44ea41e0c85633
rofinn <rory.finnegan@gmail.com>
2021-02-04 11:57:51 -0600

    Code review cleanup:
    - Remove docs reference to UncertaintySet
    - Use the unicode characters given in the PortfolioOpt.jl docs for fields
    - Add inner constructors for validating arguments (and tests).

--- src/AmbiguitySets.jl
+++ src/AmbiguitySets.jl
@@ -23,64 +23,85 @@ Base.rand(s::AmbiguitySet) = rand(distribution(s))
 # estimate. I don't think anything like that would be possible for Delague though.
 
 """
-    Bertsimas <: UncertaintySet
+    Bertsimas <: AmbiguitySet
 
 Atributes:
-- `dist::Sampleable{Multivariate, Continous}`: The parent distribution with an uncertain mean
-- `uncertainty::Array{Float64,1}` (latex notation ``\\Delta``): Uncertainty around mean. (default: std(dist) / 5)
-- `budget::Float64` (latex notation ``\\Gamma``): Number of assets in worst case. (default: 0.1 * length(dist))
+- `d::Sampleable{Multivariate, Continous}`: The parent distribution with an uncertain mean
+- `Δ::Array{Float64,1}`: Uncertainty around mean. (default: std(d) / 5)
+- `Γ::Float64`: Number of assets in worst case. (default: 0.1 * length(d))
 
 For more information on how Bertsimas' uncertainty sets are used for RO, please review
 the PortfolioOptimization.jl [docs](https://invenia.pages.invenia.ca/PortfolioOptimization.jl/).
 """
 struct Bertsimas{T<:Real, D<:Sampleable} <: AmbiguitySet{T, D}
-    dist::D
-    uncertainty::Vector{T}
-    budget::T
+    d::D
+    Δ::Vector{T}
+    Γ::T
+
+    # Inner constructor for validating arguments
+    function Bertsimas{T, D}(d::D, Δ::Vector{T}, Γ::T) where {T<:Real, D<:Sampleable}
+        length(d) == length(Δ) || throw(ArgumentError(
+            "Distribution ($(length(d))) and Δ ($(length(d))) are not the same length"
+        ))
+        all(>=(0), Δ) || throw(ArgumentError("All uncertainty deltas must be >= 0"))
+        return new{T, D}(d, Δ, Γ)
+    end
+end
+
+# Default outer constructor
+Bertsimas(d::D, Δ::Vector{T}, Γ::T) where {T<:Real, D<:Sampleable} = Bertsimas{T, D}(d, Δ, Γ)
+
+# Kwarg constructor with defaults
+function Bertsimas(
+    d::AbstractMvNormal;
+    Δ=default_bertsimas_delta(d),
+    Γ=default_bertsimas_budget(d),
+)
+    return Bertsimas(d, Δ, Γ)
 end
 
 # Not sure about these defaults, but it seems like something we should support?
-default_bertsimas_uncertainty(d::AbstractMvNormal) = sqrt.(diag(cov(d))) ./ 5
+default_bertsimas_delta(d::AbstractMvNormal) = sqrt.(var(d)) ./ 5
 default_bertsimas_budget(d::Sampleable{Multivariate}) = 0.1 * length(d)
 
-function Bertsimas(
-    d::AbstractMvNormal;
-    uncertainty=default_bertsimas_uncertainty(d),
-    budget=default_bertsimas_budget(d),
-)
-    return Bertsimas(d, uncertainty, budget)
-end
-
-distribution(s::Bertsimas) = s.dist
+distribution(s::Bertsimas) = s.d
 
 """
-    BenTal <: UncertaintySet
+    BenTal <: AmbiguitySet
 
 Atributes:
-- `dist::Sampleable{Multivariate, Continous}`: The parent distribution with an uncertain mean
-- `uncertainty::Array{Float64,1}` (latex notation ``\\Delta``): Uniform uncertainty around mean. (default: std(dist) / 5)
+- `d::Sampleable{Multivariate, Continous}`: The parent distribution with an uncertain mean
+- `Δ::Array{Float64,1}`: Uniform uncertainty around mean. (default: std(dist) / 5)
 
 For more information on how BenTal uncertainty sets are used for RO, please review
 the PortfolioOptimization.jl [docs](https://invenia.pages.invenia.ca/PortfolioOptimization.jl/).
 """
 struct BenTal{T<:Real, D<:Sampleable} <: AmbiguitySet{T, D}
-    dist::D
-    uncertainty::T
+    d::D
+    Δ::T
+
+    # Inner constructor for validating arguments
+    function BenTal{T, D}(d::D, Δ::T) where {T<:Real, D<:Sampleable}
+        Δ >= 0 || throw(ArgumentError("Uncertainty delta must be >= 0"))
+        return new{T, D}(d, Δ)
+    end
 end
 
-default_ben_tal_uncertainty(d::AbstractMvNormal) = first(sqrt.(diag(cov(d))) ./ 5)
+# Default outer constructor
+BenTal(d::D, Δ::T) where {T<:Real, D<:Sampleable} = BenTal{T, D}(d, Δ)
 
-function BenTal(d::AbstractMvNormal; uncertainty=default_ben_tal_uncertainty(d))
-    return BenTal(d, uncertainty)
-end
+# Kwarg constructor with default delta value
+BenTal(d::AbstractMvNormal; Δ=default_ben_tal_delta(d)) = BenTal(d, Δ)
 
-distribution(s::BenTal) = s.dist
+default_ben_tal_delta(d::AbstractMvNormal) = first(sqrt.(var(d)) ./ 5)
+
+distribution(s::BenTal) = s.d
 
 """
     Delague <: AmbiguitySet
 
 Atributes:
-- `dist::Sampleable{Multivariate, Continous}`: The parent distribution with an uncertain mean
+- `d::Sampleable{Multivariate, Continous}`: The parent distribution with an uncertain mean
 - `γ1::Float64`: Uniform uncertainty around the mean (has to be greater than 0). (default: std(dist) / 5)
 - `γ2::Float64`: Uncertainty around the covariance (has to be greater than 1). (default: 3.0)
 - `coefficients::Vector{Float64}`: Piece-wise utility coeficients (default [1.0]).
@@ -90,15 +111,34 @@ For more information on how BenTal uncertainty sets are used for RO, please revi
 the PortfolioOptimization.jl [docs](https://invenia.pages.invenia.ca/PortfolioOptimization.jl/).
 """
 struct Delague{T<:Real, D<:Sampleable} <: AmbiguitySet{T, D}
-    dist::D
+    d::D
     γ1::T
     γ2::T
     coefficients::Vector{T}
     intercepts::Vector{T}
+
+    # Inner constructor for validating arguments
+    function Delague{T, D}(
+        d::D, γ1::T, γ2::T, coefficients::Vector{T}, intercepts::Vector{T}
+    ) where {T<:Real, D<:Sampleable}
+        length(coefficients) == length(intercepts) || throw(ArgumentError(
+            "Length of coefficients ($(length(coefficients))) and intercepts " *
+            "($(length(intercepts))) do not match"
+        ))
+        γ1 >= 0 || throw(ArgumentError("γ1 must be >= 0"))
+        γ2 >= 1 || throw(ArgumentError("γ2 must be >= 1"))
+        return new{T, D}(d, γ1, γ2, coefficients, intercepts)
+    end
 end
 
-default_delague_γ1(d::AbstractMvNormal) = first(sqrt.(diag(cov(d))) ./ 5)
+# Default outer constructor
+function Delague(
+    d::D, γ1::T, γ2::T, coefficients::Vector{T}, intercepts::Vector{T}
+) where {T<:Real, D<:Sampleable}
+    Delague{T, D}(d, γ1, γ2, coefficients, intercepts)
+end
 
+# Kwarg constructor with defaults
 function Delague(
     d::AbstractMvNormal;
     γ1=default_delague_γ1(d), γ2=3.0, coefficients=[1.0], intercepts=[0.0],
@@ -106,7 +146,9 @@ function Delague(
     return Delague(d, γ1, γ2, coefficients, intercepts)
 end
 
-distribution(s::Delague) = s.dist
+default_delague_γ1(d::AbstractMvNormal) = first(sqrt.(diag(cov(d))) ./ 5)
+
+distribution(s::Delague) = s.d
 
 # NOTE: The Betina formulation doesn't use ambiguity sets?
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -13,21 +13,28 @@ using AmbiguitySets:
 @testset "AmbiguitySets.jl" begin
     # Mostly just some simple constructor tests
     @testset "Bertsimas" begin
-        s = Bertsimas(MvNormal(10, 0.25); uncertainty=fill(0.05, 10), budget=1.0)
+        s = Bertsimas(MvNormal(10, 0.25); Δ=fill(0.05, 10), Γ=1.0)
 
         @test isa(s, Sampleable)
         @test isa(s, AmbiguitySet)
         @test distribution(s) == MvNormal(10, 0.25)
         @test s == Bertsimas(MvNormal(10, 0.25))
+
+        # Test constructor error cases
+        @test_throws ArgumentError Bertsimas(MvNormal(10, 0.25); Δ=fill(-0.05, 10))
+        @test_throws ArgumentError Bertsimas(MvNormal(10, 0.25); Δ=fill(0.05, 9))
     end
 
     @testset "BenTal" begin
-        s = BenTal(MvNormal(10, 0.25); uncertainty=0.05)
+        s = BenTal(MvNormal(10, 0.25); Δ=0.05)
 
         @test isa(s, Sampleable)
         @test isa(s, AmbiguitySet)
         @test distribution(s) == MvNormal(10, 0.25)
         @test s == BenTal(MvNormal(10, 0.25))
+
+        # Test constructor error cases
+        @test_throws ArgumentError BenTal(MvNormal(10, 0.25); Δ=-0.5)
     end
 
     @testset "Delague" begin
@@ -44,5 +51,30 @@ using AmbiguitySets:
         @test isa(s, AmbiguitySet)
         @test distribution(s) == MvNormal(10, 0.25)
         @test s == Delague(MvNormal(10, 0.25))
+
+        # Test constructor error cases
+        @test_throws ArgumentError Delague(
+            MvNormal(10, 0.25);
+            γ1=-0.05,
+            γ2=3.0,
+            coefficients=[1.0],
+            intercepts=[0.0],
+        )
+
+        @test_throws ArgumentError Delague(
+            MvNormal(10, 0.25);
+            γ1=0.05,
+            γ2=0.5,
+            coefficients=[1.0],
+            intercepts=[0.0],
+        )
+
+        @test_throws ArgumentError Delague(
+            MvNormal(10, 0.25);
+            γ1=0.05,
+            γ2=3.0,
+            coefficients=[1.0, 0.2],
+            intercepts=[0.0],
+        )
     end
 end
